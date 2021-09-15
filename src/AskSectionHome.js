620b691e74d16be1293de955ce061923457da62d
renjval12 <reneejoyvalentine3@gmail.com>
2021-09-14 20:45:50 -0400

    update Home.js Component

--- src/AskSectionHome.js
+++ src/AskSectionHome.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function AskSection(props) {
     return (
-        <div>
+        <div id={props.id}>
             <h2>{props.askHeading}</h2>
             <p>{props.description}</p>
             <button>Go to Page</button>
--- src/Home.js
+++ src/Home.js
@@ -9,14 +9,14 @@ export default function Home() {
             <section id="services-section" >
                 <h2 class="home-section-heading">Services</h2>
                 <section className="service-select-cards">
-                    <SelectCardSection sectionName="Ask the Cards"/>
-                    <SelectCardSection sectionName="Ask the Stars"/>
-                    <SelectCardSection sectionName="Ask the Crystal Ball"/>
+                    <SelectCardSection sectionName="Ask the Cards" />
+                    <SelectCardSection sectionName="Ask the Stars" />
+                    <SelectCardSection sectionName="Ask the Crystal Ball" />
                 </section>
-                <section className="ask-cards-section">
-                    <AskSection askHeading="Ask the Cards" description="Get a 3 or 5 card tarot reading based on a question you have, with our tarot card generator."/>
-                    <AskSection askHeading="Ask the Stars" description="Get general info about your sun sign, your lucky number, and a short horoscope for the day."/>
-                    <AskSection askHeading="Ask the Crystal Ball" description="Ask a question, and the crystal ball generator will suggest a course of action."/>
+                <section className="ask-section">
+                    <AskSection id="ask-cards-desc" askHeading="Ask the Cards" description="Get a 3 or 5 card tarot reading based on a question you have, with our tarot card generator." />
+                    <AskSection id="ask-stars-desc" askHeading="Ask the Stars" description="Get general info about your sun sign, your lucky number, and a short horoscope for the day." />
+                    <AskSection id="ask-crystal-ball-desc" askHeading="Ask the Crystal Ball" description="Ask a question, and the crystal ball generator will suggest a course of action." />
                 </section>
             </section>
         </>
