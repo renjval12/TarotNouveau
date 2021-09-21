ee0a110b2cbcce650d5404fea9dff3d9b01d4ddf
renjval12 <reneejoyvalentine3@gmail.com>
2021-09-21 07:06:59 -0400

    update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,19 +1,34 @@
 import React from "react";
+import { Form, FloatingLabel } from "react-bootstrap";
+import Button from "@restart/ui/esm/Button";
+
 export default function Contact() {
     return (
         <>
+            <h1>Contact</h1>
             <form id="contact-form" action="https://formsubmit.co/30d22697b7647ac28e0f3b35c0a7b555" method="POST">
-                <h1 id="contact-us-heading" class="top-of-page-heading">Contact Us</h1>
-                <p class="input-heading">Name:</p>
-                <input type="text" placeholder="Required" name="Name" required />
-                <p class="input-heading">Email Address:</p>
-                <input type="text" placeholder="Required" name="Email Address" required />
-                <p class="input-heading">Upload a File:</p>
-                <input type="file" id="myFile" name="File Name" />
-                <p class="input-heading">Type in your inquiry:</p>
-                <textarea name="Inquiry Text" id="inquiry-text" cols="30" rows="10"
-                    placeholder="Please type your inquiry here"></textarea>
-                <button id="submit-contact-btn" type="submit">Send</button>
+                <FloatingLabel controlId="floatingName" label="Name">
+                    <Form.Control type="text" placeholder="Name" />
+                </FloatingLabel>
+                <FloatingLabel
+                    controlId="floatingInput"
+                    label="Email address"
+                    className="mb-3"
+                    required
+                >
+                    <Form.Control type="email" placeholder="name@example.com" />
+                </FloatingLabel>
+
+                <FloatingLabel controlId="floatingTextarea2" label="Comments">
+                    <Form.Control
+                        as="textarea"
+                        placeholder="Leave a comment here"
+                        style={{ height: '100px' }}
+                    />
+                </FloatingLabel>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
             </form>
         </>
     )
